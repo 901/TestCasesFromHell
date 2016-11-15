af71cd9325482f364620a27bdf2060ddeef58c4b
Heman Gandhi <hemangandhi@gmail.com>
2016-11-15 14:25:25 -0500

    Docs for the python.

--- Systems214/ProcessesVsThreads/test_enc.py
+++ Systems214/ProcessesVsThreads/test_enc.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 def partition_by(fn, it):
+    """Divides an iterable (ie. list or string)
+       into chunks based on the return value of
+       a function (fn here)."""
     prev = None
     acc = []
     for i in it:
@@ -18,6 +21,8 @@ def partition_by(fn, it):
         yield acc
 
 def group(it, ct):
+    """Groups the values in an iterable into lists
+       of count. group([1, 2, 3, 4], 2) = [[1, 2], [3, 4]]"""
     li = list(it)
     l = len(li)
     return map(lambda x: map(lambda y: y[1], x),
@@ -25,6 +30,7 @@ def group(it, ct):
                             enumerate(li)))
 
 def un_enc(enc):
+    """Decompresses a string."""
     if not enc[0].isdigit():
         enc = '1' + enc
     p2s = lambda i: i[1][0] * int(''.join(i[0])) + ''.join(i[1][1:])
@@ -33,23 +39,29 @@ def un_enc(enc):
     return s
 
 def un_enc_file(fil):
+    """Decompresses a file."""
     with open(fil) as f:
         return un_enc(f.read().strip())
 
 def un_enc_files(*files):
+    """Decompresses an abitrary number
+       of files. Puts the decompression
+       in the order that the files appear."""
     return ''.join(un_enc_file(i) for i in files)
 
 def is_dec(dec, encs):
+    """Validates everything. Prints everything out."""
     t = un_enc_files(*encs)
     print("The decompression: "+ t)
     with open(dec) as d:
         t2 = ''.join(filter(str.isalpha, d.read()))
-        print("The unencoded file without non-alphabetics: " + t2)
+        print("The unencoded file with alphabetics only: " + t2)
         return t2 == t
 
 if __name__ == "__main__":
     import sys
     if len(sys.argv) < 2:
         print("Too few arguments!")
+        print("USAGE: python[3] test_enc.py [original file] [compressed files in order ...]")
     else:
         print(is_dec(sys.argv[1], sys.argv[2:]))
